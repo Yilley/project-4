955f42d6c8c1c98444ac07b896509adadb58ab99
Benard Augustine Adakole <pheelzconnect@gmail.com>
2022-08-08 01:05:36 +0100

    added finishing touches

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=yilley/project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-p4 --image=$dockerpath --port=8080
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/udacity-p4 8080:80
